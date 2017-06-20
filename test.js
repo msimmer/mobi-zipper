424e58f3173191bdaab0e53e52feda8983f2bb10
Maxwell Simmer <maxwell.simmer@gmail.com>
2017-06-20 11:08:10 +0200

    Add tests

--- test.js
+++ test.js
@@ -1,4 +1,24 @@
+const path = require('path')
+const mobi = require('./dist').default
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+const exec = require('child_process').exec
 
-import test from 'ava'
-import path from 'path'
-import mobi from './dist/index'
+chai.should()
+chai.use(chaiAsPromised)
+
+describe('#checkForCalibre', () => {
+  it('should verify that calibre\'s ebook-convert is installed', (done) => {
+    exec('hash foo 2>/dev/null', (err) => {
+      err.message.should.match(/Command failed:/)
+      done()
+    })
+  })
+})
+
+describe('#conditionally', () => {
+  it('should execute the callback if a condition is met', () => {
+    mobi.conditionally(false).should.eventually.be.fulfilled
+    mobi.conditionally(true, Promise.resolve(1)).should.eventually.equal(1)
+  })
+})
