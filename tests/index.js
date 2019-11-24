494b17362351098ce73aed3add49a8270a5d5dc6
Maxwell Simmer <maxwell.simmer@gmail.com>
2019-11-24 15:07:17 +0100

    Updates tests

--- tests/index.js
+++ tests/index.js
@@ -8,18 +8,41 @@ chai.should()
 chai.use(chaiAsPromised)
 
 const cwd = path.join(process.cwd(), 'tests')
-const input = path.join(cwd, 'book', 'OPS', 'content.opf')
+const inputSuccess = path.join(cwd, 'book', 'OPS', 'content-success.opf')
+const inputError = path.join(cwd, 'book', 'OPS', 'content-error.opf')
 const output = path.join(cwd)
 
 describe('mobi-zipper', () => {
-  it('creates a mobi', () => {
-    return zipper.create({ input, output, clean: true }).then(() => {
-      const mobis = fs
-        .readdirSync(output)
-        .filter(f => path.extname(f) === '.mobi')
+  let origStdout = process.stdout.write
+  let origStderr = process.stderr.write
 
-      mobis.should.have.lengthOf(1)
-      mobis[0].should.match(/\d{4}\-\d{2}\-\d{2}T\d{2}\-\d{2}\-\d{2}\.\d{3}Z/)
-    })
+  before(() => {
+    process.stdout.write = () => {}
+    process.stderr.write = () => {}
+  })
+
+  after(() => {
+    process.stdout.write = origStdout
+    process.stderr.write = origStderr
+  })
+
+  it('creates a mobi', () => {
+    return zipper
+      .create({ input: inputSuccess, output, clean: true })
+      .then(() => {
+        const mobis = fs
+          .readdirSync(output)
+          .filter(f => path.extname(f) === '.mobi')
+
+        mobis.should.have.lengthOf(1)
+        mobis[0].should.match(/\d{4}\-\d{2}\-\d{2}T\d{2}\-\d{2}\-\d{2}\.\d{3}Z/)
+      })
+  })
+
+  it('rejects errors', () => {
+    return chai
+      .expect(zipper.create({ input: inputError, output, clean: true }))
+      .to.eventually.be.rejectedWith('There was an error creating the mobi')
+      .and.be.an.instanceOf(Error)
   })
 })
