ce4eb3e5796574f53c401269c71828bf3810465b
Maxwell Simmer <maxwell.simmer@gmail.com>
2019-11-24 14:18:11 +0100

    Updates tests

--- tests/index.js
+++ tests/index.js
@@ -12,16 +12,14 @@ const input = path.join(cwd, 'book', 'OPS', 'content.opf')
 const output = path.join(cwd)
 
 describe('mobi-zipper', () => {
-  it('creates a mobi', done => {
-    zipper.create({ input, output, clean: true }).then(() => {
+  it('creates a mobi', () => {
+    return zipper.create({ input, output, clean: true }).then(() => {
       const mobis = fs
         .readdirSync(output)
         .filter(f => path.extname(f) === '.mobi')
 
       mobis.should.have.lengthOf(1)
       mobis[0].should.match(/\d{4}\-\d{2}\-\d{2}T\d{2}\-\d{2}\-\d{2}\.\d{3}Z/)
-
-      done()
     })
   })
 })
